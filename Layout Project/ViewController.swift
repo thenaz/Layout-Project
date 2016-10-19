7bee4e36abbf8642aa88648a37657270d393d232
thenaz <anaz7963@740s-pl-126.local>
2016-10-19 07:40:25 -0600

    Created Layout Project

---
+++ Layout Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Layout Project
+//
+//  Created by Nazir, Ammar on 10/19/16.
+//  Copyright © 2016 Nazir, Ammar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
